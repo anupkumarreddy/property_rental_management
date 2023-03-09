12b81cd06bae63d267a9505035b901807bb1c639
Anup Reddy <kumarreddy002@gmail.com>
2023-03-09 19:06:26 +0530

    Adding list view in custom admin pages to all models

--- easy_lease/admin.py
+++ easy_lease/admin.py
@@ -2,7 +2,53 @@ from django.contrib import admin
 
 from .models import Property, Payment, Lease, Tenant
 
-admin.site.register(Tenant)
-admin.site.register(Property)
-admin.site.register(Lease)
-admin.site.register(Payment)
+
+class TenantAdmin(admin.ModelAdmin):
+    list_display = ('tenant_first_name',
+                    'tenant_last_name',
+                    'tenant_date_of_birth',
+                    'tenant_profession',
+                    'tenant_government_id',
+                    'tenant_permanent_address',
+                    'tenant_mobile_number',
+                    'tenant_description'
+                    )
+
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('property_name',
+                    'property_owner',
+                    'property_address',
+                    'property_group_name',
+                    'property_description'
+                    )
+
+
+class LeaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'lease_tenant',
+        'lease_property',
+        'lease_advance',
+        'lease_rent',
+        'lease_start_date',
+        'lease_due_date',
+        'lease_state',
+        'lease_state_node',
+        'lease_description'
+    )
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'payment_date',
+        'payment_amount',
+        'payment_due_date',
+        'payment_partial',
+        'payment_paid'
+    )
+
+
+admin.site.register(Tenant, TenantAdmin)
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Lease, LeaseAdmin)
+admin.site.register(Payment, PaymentAdmin)
--- easy_lease/models.py
+++ easy_lease/models.py
@@ -11,6 +11,9 @@ class Tenant(models.Model):
     tenant_mobile_number = models.CharField(max_length=12)
     tenant_description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.tenant_first_name + " " + self.tenant_last_name
+
 
 class Property(models.Model):
     property_name = models.CharField(max_length=50)
@@ -18,6 +21,9 @@ class Property(models.Model):
     property_address = models.CharField(max_length=500)
     property_group_name = models.CharField(max_length=50)
     property_description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.property_name
 
 
 class Lease(models.Model):
@@ -36,6 +42,9 @@ class Lease(models.Model):
     lease_state_node = models.CharField(max_length=100)
     lease_description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.lease_tenant.tenant_first_name + self.lease_property.property_name
+
 
 class Payment(models.Model):
     payment_lease = models.ForeignKey(Lease, on_delete=models.CASCADE)
@@ -44,5 +53,6 @@ class Payment(models.Model):
     payment_due_date = models.DateField()
     payment_partial = models.BooleanField()
     payment_paid = models.BooleanField()
+
 
 
