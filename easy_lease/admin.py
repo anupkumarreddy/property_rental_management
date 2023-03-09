b5ae2a93bb28bfdc613247ed1fff8f7e2b3a740a
Anup Reddy <kumarreddy002@gmail.com>
2023-03-09 18:12:55 +0530

    Registered all models with admin interface

--- easy_lease/admin.py
+++ easy_lease/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Property, Payment, Lease, Tenant
+
+admin.site.register(Tenant)
+admin.site.register(Property)
+admin.site.register(Lease)
+admin.site.register(Payment)
